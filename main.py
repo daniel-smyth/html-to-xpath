b02a33fccd551eb0feac23c51cdf27806fb5f3ac
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2022-12-02 17:55:17 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import codecs
+from io import StringIO
+from typing import List
+from lxml import etree
+
+
+def generate_xpath_query(attribute_id: str, html: str):
+    """
+    Create array of XPaths from a HTML, searching HTML for a attribute_id
+
+    Args:
+        `attribute_id`: Zyft ID to search with
+        `page_html`: HTML to generate query from
+
+    Returns:
+        `x_paths`: List of XPaths
+    """
+    x_paths: List[str] = []
+
+    # Parse HTML to ElementTree with lxml
+    parser = etree.HTMLParser()
+    tree = etree.parse(StringIO(html), parser)
+
+    # Iter all elements of the ElementTree
+    for element in tree.iter():
+
+        # Iter the attributes of each element
+        for attribute, value in element.items():
+
+            # Check if element has a zyft_node_id
+            if attribute == "zyft_node_id" and value == attribute_id:
+
+                # This is the parent path
+                path = tree.getpath(element)
+
+                x_paths.append(path)
+
+                # Sub elements of the parent path
+                for sub_element in element:
+                    sub_element_path = tree.getpath(sub_element)
+
+                    x_paths.append(sub_element_path)
+
+                break
+        else:
+            continue  # only executed if the inner loop did NOT break
+        break  # only executed if the inner loop DID break
+
+    return x_paths
+
+
+def load_html():
+    html_stream = codecs.open("sample-html.html", "r")
+    html = html_stream.read()
+
+    x_paths = generate_xpath_query(attribute_id="123456", html=html)
+
+    print("\nX Paths array:")
+    print(x_paths, "\n")
+
+
+if __name__ == "__main__":
+    load_html()
